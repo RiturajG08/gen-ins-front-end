56e70fb37e743782fa01154afb7f251675720dd8
Abhiruchi <shendkarabhiruchi@gmail.com>
2021-04-11 12:41:41 +0530

    Buy Insurance Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ClaimInsuranceComponent } from './claim-insurance/claim-insurance.compo
 import { HomeComponent } from './home/home.component';
 import { BuyInsuranceComponent } from './buy-insurance/buy-insurance.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
