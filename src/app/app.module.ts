b3437dc8fc90a855ff5991972d8c5e259c5a31f1
Abhiruchi <shendkarabhiruchi@gmail.com>
2021-04-12 22:04:33 +0530

    navbar components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { HomeComponent } from './home/home.component';
 import { BuyInsuranceComponent } from './buy-insurance/buy-insurance.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HelpComponent } from './help/help.component';
+import { FaqComponent } from './faq/faq.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,11 @@ import { FormsModule } from '@angular/forms';
     ClaimInsuranceComponent,
     HomeComponent,
     BuyInsuranceComponent,
-    LoginComponent
+    LoginComponent,
+    ContactUsComponent,
+    AboutUsComponent,
+    HelpComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
