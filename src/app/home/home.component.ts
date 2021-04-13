bdfd82e91995dab057c00ba43df35fbbd35d92e2
Rituraj <riturajgujare01@gmail.com>
2021-04-13 17:02:10 +0530

    home page changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'home',
+  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) { }
 
-  constructor() { }
+  loggedUserId:String = null;
 
   ngOnInit(): void {
+    this.loggedUserId = sessionStorage.getItem("customerId");
   }
 
+  onBuyClick(){
+    this.router.navigateByUrl("/buy-insurance");
+  }
+
+  onRenewClick(){
+    this.router.navigateByUrl("/renew-insurance");
+  }
+
+  onClaimClick(){
+      this.router.navigateByUrl("/claim-insurance");
+    }
+
+  /*onCalculateClick(){
+    this.router.navigateByUrl("/calculate-premium");
+  }*/
 }
