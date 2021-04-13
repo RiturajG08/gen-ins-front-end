773a5219d00108be1c88c476faa6ab86a80d7d5f
harshit <harshitsingh0608@gmail.com>
2021-04-13 15:56:05 +0530

    harshit error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { BuyInsuranceComponent } from './buy-insurance/buy-insurance.component';
-import { ClaimInsuranceComponent } from './claim-insurance/claim-insurance.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
-import { FaqComponent } from './faq/faq.component';
-import { HelpComponent } from './help/help.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RenewInsuranceComponent } from './renew-insurance/renew-insurance.component';
+
+    
+import {​​​​​ NgModule }​​​​​ from'@angular/core';
+import {​​​​​ RouterModule, Routes }​​​​​ from'@angular/router';
+import {​​​​​ AboutUsComponent }​​​​​ from'./about-us/about-us.component';
+import {​​​​​ BuyInsuranceComponent }​​​​​ from'./buy-insurance/buy-insurance.component';
+import {​​​​​ ClaimInsuranceComponent }​​​​​ from'./claim-insurance/claim-insurance.component';
+import {​​​​​ ContactUsComponent }​​​​​ from'./contact-us/contact-us.component';
+import {​​​​​ CustomerRegisterComponent }​​​​​ from'./customer-register/customer-register.component';
+import {​​​​​ FaqComponent }​​​​​ from'./faq/faq.component';
+import {​​​​​ HelpComponent }​​​​​ from'./help/help.component';
+import {​​​​​ HomeComponent }​​​​​ from'./home/home.component';
+import {​​​​​ LoginComponent }​​​​​ from'./login/login.component';
+import {​​​​​ RenewInsuranceComponent }​​​​​ from'./renew-insurance/renew-insurance.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'buy-insurance', component: BuyInsuranceComponent},
-  {path: 'renew-insurance', component: RenewInsuranceComponent},
-  {path: 'claim-insurance', component: ClaimInsuranceComponent},
-  {path: 'about-us', component: AboutUsComponent},
-  {path: 'help', component: HelpComponent},
-  {path: 'contact-us', component: ContactUsComponent},
-  {path: 'faq', component: FaqComponent}
-  
+  {​​​​​path:'', component:HomeComponent}​​​​​,
+  {​​​​​path:'login', component:LoginComponent}​​​​​,
+  {​​​​​path:'buy-insurance', component:BuyInsuranceComponent}​​​​​,
+  {​​​​​path:'renew-insurance', component:RenewInsuranceComponent}​​​​​,
+  {​​​​​path:'claim-insurance', component:ClaimInsuranceComponent}​​​​​,
+  {​​​​​path:'about-us', component:AboutUsComponent}​​​​​,
+  {​​​​​path:'help', component:HelpComponent}​​​​​,
+  {​​​​​path:'contact-us', component:ContactUsComponent}​​​​​,
+  {​​​​​path:'faq', component:FaqComponent}​​​​​,
+  {​​​​​path:'customer-register', component:CustomerRegisterComponent}​​​​​
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+@NgModule({​​​​​
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
+}​​​​​)
+export class AppRoutingModule {​​​​​ }​​​​​
