ff72c46eb2811f7b177d1c034ae4b791b9e322a9
Rituraj <riturajgujare01@gmail.com>
2021-04-20 16:41:46 +0530

    adding policy

--- src/app/policy/policy.component.ts
+++ src/app/policy/policy.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DepreciationDto, Vehicle } from '../buy-insurance/buy-insurance.component';
+import { Customer } from '../customer-register/customer-register.component';
+import { PolicyService } from '../policy.service';
 
 @Component({
   selector: 'policy',
@@ -8,13 +12,28 @@ import { Component, OnInit } from '@angular/core';
 export class PolicyComponent implements OnInit {
 
   policy: Policy= new Policy();
-  constructor() { }
+  
+  constructor(private router: Router, private service: PolicyService) { }
 
   ngOnInit(): void {
   }
+
+  addPolicy(){
+    if(this.policy.period == "One Year"){
+      this.policy.startDate
+    }
+  }
+
+
 }
 
 export class Policy{
+  public id: number;
   type: String;
+  public period: String;
   startDate: String;
+  endDate: String;
+  customer: Customer= new Customer();
+  vehicle: Vehicle= new Vehicle();
+  depreciationDto: DepreciationDto= new DepreciationDto();
 }
