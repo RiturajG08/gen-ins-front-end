b7bae5f238557b1fb60a0da9f223834388bcaad6
harshit <harshitsingh0608@gmail.com>
2021-04-20 12:18:45 +0530

    depreciation frontend chnage done

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vehicle } from './buy-insurance/buy-insurance.component';
+import { DepreciationDto } from './buy-insurance/buy-insurance.component';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,13 @@ export class VehicleService {
 
   constructor(private http: HttpClient) { }
 
-  register(vehicle: Vehicle): Observable<object>{
+  addVehicle(vehicle: Vehicle): Observable<object>{
     let url= "http://localhost:8484/vehicle";
     return this.http.post(url, vehicle);
   }  
 
-  
+  addDepreciation(depreciationDto : DepreciationDto): Observable<object>{
+    let url= "http://localhost:8484/depreciation";
+    return this.http.post(url, depreciationDto);
+  }  
 }
