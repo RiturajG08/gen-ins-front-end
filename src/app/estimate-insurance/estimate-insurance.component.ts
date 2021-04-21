c802cdae72971fe26b89ee0486a99095eaa3315e
harshit <harshitsingh0608@gmail.com>
2021-04-21 18:55:46 +0530

    some chnage in istimate calculation of premium

--- src/app/estimate-insurance/estimate-insurance.component.ts
+++ src/app/estimate-insurance/estimate-insurance.component.ts
@@ -13,24 +13,27 @@ export class EstimateInsuranceComponent  {
   public vmodel: string;
   public drep: number;
   public idv:number;
+  public month: number;
+  
 
   getpremium(){
-    if(this.age<=1){
+    this.month=this.age*12;
+    if(this.month<=12){
           this.drep=this.price*0.1;
           this.idv=this.price-this.drep;
           this.premium=this.idv*4/100;
     }
-    if(this.age>=2 && this.age<3){
+    if(this.month>12 && this.month<=24){
       this.drep=this.price*0.2;
       this.idv=this.price-this.drep;
       this.premium=this.idv*4/100;
     }
-    if(this.age>=3 && this.age<4){
+    if(this.month>24 && this.month<=36){
       this.drep=this.price*0.3;
       this.idv=this.price-this.drep;
       this.premium=this.idv*4/100;
     }
-    else if(this.age>4){
+    else if(this.month>36){
       this.drep=this.price*0.5;
       this.idv=this.price-this.drep;
       this.premium=this.idv*4/100;
